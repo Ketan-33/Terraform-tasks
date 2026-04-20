e6c36e6f8dc2cd0890c4451be91e2c437221999b
Ketan Pardhi <ketanpardhi04@gmail.com>
2026-04-20 15:56:04 +0530

    fmt

--- task06/main.tf
+++ task06/main.tf
@@ -1,39 +1,39 @@
-data "azurerm_key_vault" "existing"{
-    name =var.kv_name
-    resource_group_name = var.kv_resource_group_name
+data "azurerm_key_vault" "existing" {
+  name                = var.kv_name
+  resource_group_name = var.kv_resource_group_name
 }
 
-resource "azurerm_resource_group" "this"{
-    name = local.rg_name
-    location = var.location
-    tags = var.tags
+resource "azurerm_resource_group" "this" {
+  name     = local.rg_name
+  location = var.location
+  tags     = var.tags
 }
 
-module "sql"{
-    source = "./modules/sql"
+module "sql" {
+  source = "./modules/sql"
 
-    resource_group_name = azurerm_resource_group.this.name
-    location = azurerm_resource_group.this.location
-    sql_server_name = local.sql_server_name
-    sql_db_name = local.sql_db_name
-    sql_db_sku = var.sql_db_sku
-    sql_admin_username = var.sql_admin_username
-    firewall_rule_name = var.sql_firewall_rule_name
-    allowed_ip_address = var.allowed_ip_address
-    key_vault_name = var.kv_name
-    kv_secret_name_sql_admin = var.kv_secret_name_sql_admin
-    kv_secret_name_sql_password = var.kv_secret_name_sql_password
-    tags = var.tags
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  sql_server_name             = local.sql_server_name
+  sql_db_name                 = local.sql_db_name
+  sql_db_sku                  = var.sql_db_sku
+  sql_admin_username          = var.sql_admin_username
+  firewall_rule_name          = var.sql_firewall_rule_name
+  allowed_ip_address          = var.allowed_ip_address
+  key_vault_name              = var.kv_name
+  kv_secret_name_sql_admin    = var.kv_secret_name_sql_admin
+  kv_secret_name_sql_password = var.kv_secret_name_sql_password
+  tags                        = var.tags
 }
 
-module "webapp"{
-    source ="./modules/webapp"
+module "webapp" {
+  source = "./modules/webapp"
 
-    resource_group_name = azurerm_resource_group.this.name
-    location = azurerm_resource_group.this.location
-    asp_name = local.asp_name
-    asp_sku = var.asp_sku
-    dotnet_version = var.dotnet_version
-    sql_connection_string = module.sql.sql_connection_string
-    tags = var.tags
+  resource_group_name   = azurerm_resource_group.this.name
+  location              = azurerm_resource_group.this.location
+  asp_name              = local.asp_name
+  asp_sku               = var.asp_sku
+  dotnet_version        = var.dotnet_version
+  sql_connection_string = module.sql.sql_connection_string
+  tags                  = var.tags
 }
--- task06/modules/webapp/variables.tf
+++ task06/modules/webapp/variables.tf
@@ -1,33 +1,33 @@
 variable "resource_group_name" {
-    description = "Name of the resource group"
-    type        = string
+  description = "Name of the resource group"
+  type        = string
 }
 variable "location" {
-    description = "Azure region for resource deployment"
-    type        = string
+  description = "Azure region for resource deployment"
+  type        = string
 }
 variable "asp_name" {
-    description = "Name of the App Service Plan"
-    type        = string
+  description = "Name of the App Service Plan"
+  type        = string
 }
 variable "asp_sku" {
-    description = "SKU for the App Service Plan"
-    type        = string
+  description = "SKU for the App Service Plan"
+  type        = string
 }
-varibable "app_name" {
-    description = "Name of the App Service"
-    type        = string
+variable "app_name" {
+  description = "Name of the App Service"
+  type        = string
 }
 variable "dotnet_version" {
-    description = "Version of .NET for the App Service"
-    type        = string
+  description = "Version of .NET for the App Service"
+  type        = string
 }
 variable "sql_connection_string" {
-    description = "Connection string for the SQL Database"
-    type        = string
-    sensitive   = true
+  description = "Connection string for the SQL Database"
+  type        = string
+  sensitive   = true
 }
-variable "tags" {   
-    description = "Tags to apply to resources"
-    type        = map(string)
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
 }
