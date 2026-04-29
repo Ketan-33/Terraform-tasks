942569e205a779a4ab50de70b1c49344c4a5ed33
Ketan Pardhi <ketanpardhi04@gmail.com>
2026-04-29 10:20:08 +0530

    fix

--- task08/modules/redis/main.tf
+++ task08/modules/redis/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_redis_cache" "redis" {
-  name                = var.redis_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  capacity            = 2
-  family              = "C"
+  name                 = var.redis_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  capacity             = 2
+  family               = "C"
   sku_name             = "Basic"
   non_ssl_port_enabled = false # SSL only — port 6380
 
