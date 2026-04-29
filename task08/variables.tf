2b6bc41dc9d3b5b8aedd29f2e447224aeeb90263
Ketan Pardhi <ketanpardhi04@gmail.com>
2026-04-29 10:04:20 +0530

    fmt

--- task08/variables.tf
+++ task08/variables.tf
@@ -1,13 +1,13 @@
 variable "name_prefix" {
   description = "Prefix for all resource names"
-  type    = string
-  default = "cmtr-9f1znn32-mod8"
+  type        = string
+  default     = "cmtr-9f1znn32-mod8"
 }
 
 variable "location" {
   description = "Azure region for resource deployment"
-  type    = string
-  default = "eastus"
+  type        = string
+  default     = "eastus"
 }
 
 variable "git_pat" {
@@ -18,7 +18,7 @@ variable "git_pat" {
 
 variable "tags" {
   description = "Tags to be applied to all resources"
-  type = map(string)
+  type        = map(string)
   default = {
     Creator = "ketan_thansinghpardhi@epam.com"
   }
